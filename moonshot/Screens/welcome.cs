686fa04cadd0bec2dfcf677ef5f474afc9c8bc73
Mia <Mia@DESKTOP-DIOFRG1>
2020-11-06 19:44:13 -0600

    Updates...

--- moonshot/Screens/welcome.cs
+++ moonshot/Screens/welcome.cs
@@ -10,20 +10,45 @@ namespace moonshot.Screens
         public override void Display()
         {
             starscape();
-            DrawText("Welcome to Moonshot!" + Settings.DefaultWidth, 220, 200, 40, MAROON);
+            DrawText("Welcome to Moonshot!", 220, 200, 40, MAROON);
         }
         private static void starscape()
         {
             int Width = GetScreenWidth();
             int Height = GetScreenHeight();
 
-            for (int w = 0; w < Width; w=w+10)
+            for (int w = 0; w < Width; w=NumberGenerator(w, 50, 200))
             {
-                for (int h = 0; h < Height; h=h+10)
+                for (int h = 0; h < Height; h=NumberGenerator(h, 10, 100))
                 {
-                    Raylib.DrawCircle(h, w, 1, WHITE);
+                    Random random = new Random(w+h);
+                    Raylib.DrawCircle(h, w, random.Next(1,10), WHITE);
                 }
             }
+            for (int w = 0; w < Width; w = NumberGenerator(w, 10, 100))
+            {
+                for (int h = 0; h < Height; h = NumberGenerator(h, 50, 200))
+                {
+                    Random random = new Random(w + h);
+                    Raylib.DrawCircle(h, w, random.Next(1, 5), BLUE);
+                }
+            }
+            for (int w = 0; w < Width; w = NumberGenerator(w, 100, 150))
+            {
+                for (int h = 0; h < Height; h = NumberGenerator(h, 100, 150))
+                {
+                    Random random = new Random(w + h);
+                    Raylib.DrawCircle(h, w, random.Next(5, 10), YELLOW);
+                }
+            }
+        }
+
+        private static int NumberGenerator(int inputNumber, int minIncrease, int maxIncrease)
+        {
+            Random random = new Random(inputNumber);
+            int rn = random.Next(((int)inputNumber + minIncrease), ((int)inputNumber + maxIncrease));
+            Console.WriteLine(inputNumber + " " + rn);
+            return rn;
         }
     }
 }
